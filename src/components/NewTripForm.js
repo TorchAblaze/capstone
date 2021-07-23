e6bbf5d4c68965dd392f6e63b5a0fde88e443457
TorchAblaze <tiffanygreathead@gmail.com>
2021-07-23 16:19:19 -0700

    Add input fields and input styling

--- src/components/NewTripForm.js
+++ src/components/NewTripForm.js
@@ -6,7 +6,7 @@ function NewTripForm(props) {
   function handleNewTripFormSubmission(event) {
     event.preventDefault();
     props.onNewTripCreation({
-      destination: event.target.destination.value,
+      name: event.target.name.value,
       // photo: event.target.photo.value,
       id: v4(),
     });
@@ -15,14 +15,39 @@ function NewTripForm(props) {
     <React.Fragment>
       <hr />
       <form onSubmit={handleNewTripFormSubmission}>
-        <input
-          type="text"
-          name="destination"
-          placeholder="Name your trip"
-          required
-        />
+        <label for="name">Trip Name</label>
+        <input type="text" name="name" placeholder="Name your trip" required />
+        <fieldset>
+          <legend>Trip Dates</legend>
+          <label for="start-date" className="trip-date">
+            Start Date
+          </label>
+          <input
+            type="date"
+            name="start-date"
+            className="trip-date"
+            placeholder="Trip Start Date"
+            required
+          />
+          <label for="end-date" className="trip-date">
+            End Date
+          </label>
+          <input
+            type="date"
+            name="end-date"
+            className="trip-date"
+            placeholder="Trip End Date"
+            required
+          />
+        </fieldset>
+        <label for="tips">Tips</label>
+        <input type="text" name="tips" placeholder="Travel Tips" />
         {/* <input type="image" name="photo" /> */}
         {/* Add button that will let you upload more photos */}
+        <label for="photo-date">Date</label>
+        <input type="date" name="photo-date" placeholder="Date of Photo" />
+        <label for="caption">Caption</label>
+        <input type="text" name="caption" placeholder="Photo Caption" />
         <br />
         <button type="submit">Add Trip</button>
       </form>
