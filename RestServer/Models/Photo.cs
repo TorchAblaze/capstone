559ff616a5b6c08d369d24934c966ae6385cddf8
TorchAblaze <tiffanygreathead@gmail.com>
2021-07-23 10:26:02 -0700

    Add Photo Model for trip photo info

---
+++ RestServer/Models/Photo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestServer.Models
+{
+  public class Photo
+  {
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public DateTime StartDate { get; set; } = DateTime.Now;
+    public DateTime EndDate { get; set; } = DateTime.Now;
+    public string Description { get; set; }
+    public string TripTips { get; set; }
+  }
+}
