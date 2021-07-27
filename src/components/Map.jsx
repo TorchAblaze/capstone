a07703b8fd722806acc86f14796eaed1d75cab74
TorchAblaze <tiffanygreathead@gmail.com>
2021-07-26 23:52:59 -0700

    Add Map marker when clicked functionality

---
+++ src/components/Map.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import { API_KEY } from "../api.js";
+
+const containerStyle = {
+  width: "100%",
+  height: "600px",
+};
+
+const center = {
+  lat: 40.56,
+  lng: -102.021,
+};
+
+const Map = () => {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: API_KEY,
+  });
+
+  const [position, setPosition] = useState(undefined);
+
+  const handleClick = (e) => {
+    console.log(`Handling your click!`);
+    const { lat, lng } = e.latLng;
+    setPosition({
+      lat: lat(),
+      lng: lng(),
+    });
+  };
+
+  console.log(`Rendering through a map component`);
+
+  return isLoaded ? (
+    <GoogleMap
+      onClick={handleClick}
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+    >
+      {position ? <Marker position={position} /> : undefined}
+    </GoogleMap>
+  ) : (
+    <div>Loading...</div>
+  );
+};
+
+export default React.memo(Map);
