da5c14edd00bd7a755568b09f328651d835ae401
TorchAblaze <tiffanygreathead@gmail.com>
2021-07-26 23:53:16 -0700

    Fix: Remove comments from App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,16 @@
 import React from "react";
 import Header from "./Header";
 import TripControl from "./TripControl";
-import Pin from "./Pin";
+import Map from "./Map";
 
 function App() {
   return (
     <React.Fragment>
-      {/* <Router> */}
       <Header />
-      {/* <Switch>
-         <Route path="signin">
-           <Signin />
-         </Route>
-         <Route path="/"> */}
-      <Pin />
+      <Map />
       <TripControl />
-      {/* </Route>
-       </Switch>
-     </Router> */}
     </React.Fragment>
   );
 }
 
 export default App;
-
-// API CALL  <script async defer src="https://maps.googleapis.com/maps/api/js?key=API_KEY&callback=initMap"></script>
