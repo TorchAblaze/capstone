9b3fd8922432ec2d9c0d624793ee0c9f731a4a65
TorchAblaze <tiffanygreathead@gmail.com>
2021-07-29 02:44:04 -0700

    Add react routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import Header from "./Header";
 import TripControl from "./TripControl";
 import Map from "./Map";
+import { BrowserRouter, Route } from "react-router-dom";
+import MapControl from "./MapControl";
 
 function App() {
   return (
     <React.Fragment>
-      <Header />
-      <Map />
-      <TripControl />
+      <BrowserRouter>
+        <Header />
+        <Route exact path="/" component={Map} />
+        <Route exact path="/trips" component={TripControl} />
+        <Route exact path="/trip-form" component={MapControl} />
+      </BrowserRouter>
     </React.Fragment>
   );
 }
