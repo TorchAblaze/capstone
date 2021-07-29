b3fa0fa21a97f3791ae457a4ff1d1c4920c11575
TorchAblaze <tiffanygreathead@gmail.com>
2021-07-29 02:45:46 -0700

    Add spliced trip start and end dates

--- src/components/TripDetail.js
+++ src/components/TripDetail.js
@@ -1,29 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
+import StaticMap from "./StaticMap";
 
 function TripDetail(props) {
   const { trip } = props;
 
   return (
     <React.Fragment>
+      <StaticMap />
       <hr />
-      <h1>{trip.name}</h1>
+      <h2>{trip.name}</h2>
       <p>
-        {trip.startDate} - {trip.endDate}
+        <strong>{trip.startDate.slice(0, 10)}</strong> -{" "}
+        <strong>{trip.endDate.slice(0, 10)}</strong>
       </p>
       <h3>Things to keep in mind:</h3>
       <p>{trip.tips}</p>
 
       <h3>Memories</h3>
-      <img src={trip.photo1} alt={trip.caption1} />
+
+      <img className="trip-image" src={trip.photo1} alt={trip.caption1} />
       <p>{trip.photoDate1}</p>
       <p>{trip.caption1}</p>
 
-      <img src={trip.photo2} alt={trip.caption2} />
+      <img className="trip-image" src={trip.photo2} alt={trip.caption2} />
       <p>{trip.photoDate2}</p>
       <p>{trip.caption2}</p>
 
-      <img src={trip.photo3} alt={trip.caption3} />
+      <img className="trip-image" src={trip.photo3} alt={trip.caption3} />
       <p>{trip.photoDate3}</p>
       <p>{trip.caption3}</p>
       <hr />
